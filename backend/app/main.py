4a78b83ab8e2d72a8e4ea8a61e956c9542e22b82
Richard <richard.sinaga@berca.co.id>
2025-10-30 16:15:40 +0700

    add google router

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from typing import Union
 from app.db import base
 from fastapi import FastAPI
 from app.db.session import engine
-from app.routers import apple
+from app.routers import apple, google
 
 app = FastAPI(title="Simple Stock Analysis")
 
@@ -13,3 +13,4 @@ def read_root():
     return {"Hello": "World"}
 
 app.include_router(apple.router)
+app.include_router(google.router)
