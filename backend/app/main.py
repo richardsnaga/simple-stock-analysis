8a3d4536d7eb240422f35d3077871060061abe0d
Richard <richard.sinaga@berca.co.id>
2025-10-30 15:40:57 +0700

    remove items example

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,9 +12,4 @@ base.Base.metadata.create_all(bind=engine)
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
 app.include_router(apple.router)
