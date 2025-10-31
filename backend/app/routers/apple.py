e96212356535911185ee396624b1f79e808b99ba
Richard <richard.sinaga@berca.co.id>
2025-10-31 17:32:19 +0700

    add api prefix

--- backend/app/routers/apple.py
+++ backend/app/routers/apple.py
@@ -6,9 +6,10 @@ from app.schemas.apple import AppleCreate, AppleResponse
 from typing import List
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
+from fastapi import Request
 
 
-router = APIRouter(prefix="/apples", tags=["apples"])
+router = APIRouter(prefix="/api/apples", tags=["apples"])
 
 def get_db():
     db = SessionLocal()
