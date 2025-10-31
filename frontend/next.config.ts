3876719ca640fbc9cef2006a41acc209b0250ee1
Richard <richard.sinaga@berca.co.id>
2025-10-31 22:34:12 +0700

    comment rewrites

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,15 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:8000/api/:path*",
-      },
-    ];
-  },
+  output: 'export',
+  // Production di comment
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       // destination: "http://localhost:8000/api/:path*", // dev
+  //       destination: "http://backend:8000/api/:path*", // production
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
