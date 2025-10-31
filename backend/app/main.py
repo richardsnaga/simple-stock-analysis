c2b0d58667adf989b39f7ed86594491394baa046
Richard <richard.sinaga@berca.co.id>
2025-10-31 16:46:51 +0700

    add cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,9 +2,24 @@ from typing import Union
 from app.db import base
 from fastapi import FastAPI
 from app.db.session import engine
-from app.routers import apple, google
+from app.routers import apple, google, returns
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Simple Stock Analysis")
+
+origins = [
+    "http://localhost:3000",  # frontend Next.js dev
+    "http://127.0.0.1:3000",  # kadang pakai IP
+    # tambahkan domain production di sini
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 base.Base.metadata.create_all(bind=engine)
 
@@ -14,3 +29,4 @@ def read_root():
 
 app.include_router(apple.router)
 app.include_router(google.router)
+app.include_router(returns.router)
